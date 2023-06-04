db3b10eb29d59381ff6283dd5c81b41575eccf7a
newtoncutrim <newtoncutrim777@gmail.com>
2023-06-04 11:09:55 -0300

    template

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,9 +8,59 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500&display=swap" rel="stylesheet">
+    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous"> --}}
     <link rel="stylesheet" href="/asset/css/style.css">
 </head>
 <body>
-a
+    <div class="container">
+        <div class="sidebar">
+            logo
+        </div>
+        <div class="content">
+            <nav>
+                <a href="#" class="btn btn-primary">Criar tarefa</a>
+            </nav>
+            <main>
+                <section class="graph">
+                    <div class="graph_header">
+                        <h2>Progresso do dia</h2>
+                        <hr class="linhaHeader"/>
+                        Data
+                    </div>
+                    <div class="graph_header_subititle">
+                        Tarefas: 3/6
+                    </div>
+                    <div class="graph_placeholder">
+
+                    </div>
+                    <p class="graph_header-tasks_left">Restam 3 tarefas a serem realizadas</p>
+                </section>
+                <section class="lista">
+                    <div class="list-header">
+                        <select class="list-header-select">
+                            <option value="1">Todas as tarefas</option>
+                        </select>
+                    </div>
+                    <div class="task-list">
+                        <div class="task">
+                            <div class="title">
+                                <input type="checkbox">
+                                <div class="task_title">Titulo da tarefa</div>
+                            </div>
+                            <div class="priority">
+                                <div class="sphere"></div>
+                                <div>Titulo da tarefa</div>
+                            </div>
+                            <div class="actions">
+                                Editar - Excluir
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </main>
+        </div>
+    </div>
+
+    {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script> --}}
 </body>
 </html>
