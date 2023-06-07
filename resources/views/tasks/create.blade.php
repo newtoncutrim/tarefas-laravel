f6ed0a0b09b1d3ff77f0a2422114051b1e6e03bf
newtoncutrim <newtoncutrim777@gmail.com>
2023-06-07 20:00:46 -0300

    formulario de adicionar tarefa

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -10,13 +10,28 @@
                 <label for="title">
                     Titulo da Tarefa
                 </label>
-                <input type="text" name="title" required placeholder="digite titulo da tarefa">
+                <input type="text" id="title" name="title" required placeholder="digite titulo da tarefa">
             </div>
             <div class="inputArea">
-                <label for="title">
-                    Titulo da Tarefa
+                <label for="due_date">
+                    Data da Realização
                 </label>
-                <input type="text" name="title" required placeholder="digite titulo da tarefa">
+                <input type="date" id="due_date" name="due_date" required placeholder="digite titulo da tarefa">
+            </div>
+            <div class="inputArea">
+                <label for="category">
+                    Categoria
+                </label>
+                <select id="category" name="category" required>
+                    <option selected disabled>Selecione a Categoria</option>
+                    <option value=""></option>
+                </select>
+            </div>
+            <div class="inputArea">
+                <label for="description">
+                    Descrição da Tarefa
+                </label>
+                <textarea id="description" name="description" placeholder="Digite a descrição da Tarefa" ></textarea>
             </div>
         </form>
     </section>
